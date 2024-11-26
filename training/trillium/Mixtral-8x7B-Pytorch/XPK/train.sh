f9bb1be0cae15965d473f7daca68b02d046fe2f7
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-11-26 15:10:06 -0800

    Update train.sh

--- training/trillium/Mixtral-8x7B-Pytorch/XPK/train.sh
+++ training/trillium/Mixtral-8x7B-Pytorch/XPK/train.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 # source /app/env.sh
-source env.sh
+# source env.sh
+export ZONE=us-east5-c
+export PROJECT=tpu-prod-env-automated
+export TPU_TYPE=v6e-256
+export NUM_SLICE=1
+export CLUSTER_NAME=bodaborg-v6e-256 # use existing CLUSTER if you have
+
+# Environment variables associated with training config.
+export BATCH_PER_DEVICE=4
+export SEQUENCE_LENGTH=4096
+export MAX_STEP=50
+export WORKLOAD_NAME=${USER}-xpk-${TPU_TYPE}-nov261457pm # Your workload name. Need to update for different run.
+export BASE_DOCKER_IMAGE=us-central1-docker.pkg.dev/deeplearning-images/reproducibility/pytorch-tpu-mixtral:v1
+export PROFILE_LOG_DIR=gs://manfei_bucket_automated # GCS bucket to store profile in form of gs://...
+export HF_TOKEN=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 # Add your own Hugging face token to download model
+
+
 
 # Extract the number after '-' in TPU_TYPE
 TPU_NUM=$(echo "$TPU_TYPE" | grep -oP '(?<=-)\d+')
@@ -25,7 +41,8 @@ export PROFILE_DURATION_MS=100000
 export PROFILE_LOGDIR=${PROFILE_LOG_DIR}
 export XLA_PERSISTENT_CACHE_PATH=/app/xla_cache/
 export TPU_LIBRARY_PATH=/workspace/_libtpu.so
-export NUM_TPU_SLICE=${NUM_SLICE}
+export NUM_TPU_SLICE=1 # ${NUM_SLICE}
+echo "NUM_TPU_SLICE=$NUM_TPU_SLICE"
 export LIBTPU_INIT_ARGS="--xla_tpu_enable_flash_attention=false --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true --xla_tpu_scoped_vmem_limit_kib=81920"
 
 huggingface-cli login --token=${HF_TOKEN}
