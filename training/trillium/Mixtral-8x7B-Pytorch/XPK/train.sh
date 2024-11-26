6b055dd69406a7754dc53bf65370771a3add3a3d
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-11-26 15:31:18 -0800

    Update train.sh

--- training/trillium/Mixtral-8x7B-Pytorch/XPK/train.sh
+++ training/trillium/Mixtral-8x7B-Pytorch/XPK/train.sh
@@ -48,16 +48,17 @@ export LIBTPU_INIT_ARGS="--xla_tpu_enable_flash_attention=false --xla_tpu_enable
 huggingface-cli login --token=${HF_TOKEN}
 
 echo "CURRENT_PATH=$PWD"
-ls -l
-echo "all files"
+# ls -l
+# tpu-recipes / transformers
+# echo "all files"
 
 # Note --per_device_train_batch_size is the global batch size since we overwrite the dataloader in the HF trainer.
 python3 /workspace/transformers/examples/pytorch/language-modeling/run_clm.py \
 --dataset_name=wikitext --dataset_config_name=wikitext-103-raw-v1 \
 --per_device_train_batch_size=${GLOBAL_BATCH_SIZE} --do_train --output_dir=test-clm \
---overwrite_output_dir --config_name=./config.json \
+--overwrite_output_dir --config_name=/workspace/tpu-recipes/training/trillium/Mixtral-8x7B-Pytorch/XPK/config.json \
 --cache_dir=cache --tokenizer_name=mistralai/Mixtral-8x7B-v0.1 \
 --block_size=${SEQUENCE_LENGTH} --optim=adafactor --save_strategy=no \
 --logging_strategy=no --fsdp="full_shard" \
---fsdp_config=./fsdp_config.json --torch_dtype=bfloat16 \
+--fsdp_config=/workspace/tpu-recipes/training/trillium/Mixtral-8x7B-Pytorch/XPK/fsdp_config.json --torch_dtype=bfloat16 \
 --dataloader_drop_last=yes --max_steps=${MAX_STEP} --gmm --flash_attention
