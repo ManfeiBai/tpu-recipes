9923a7f607b904ff9ace40c15360f68c4cf1035a
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-11-26 14:59:15 -0800

    Update env.sh

--- training/trillium/Mixtral-8x7B-Pytorch/XPK/env.sh
+++ training/trillium/Mixtral-8x7B-Pytorch/XPK/env.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Environment variables associated with XPK on GCP.
-export ZONE=...
-export PROJECT=...
+export ZONE=us-east5-c
+export PROJECT=tpu-prod-env-automated
 export TPU_TYPE=v6e-256
 export NUM_SLICE=1
-export CLUSTER_NAME=xpk-$USER-... # use existing CLUSTER if you have
+export CLUSTER_NAME=bodaborg-v6e-256 # use existing CLUSTER if you have
 
 # Environment variables associated with training config.
 export BATCH_PER_DEVICE=4
 export SEQUENCE_LENGTH=4096
 export MAX_STEP=50
-export WORKLOAD_NAME=${USER}-xpk-${TPU_TYPE}-... # Your workload name. Need to update for different run.
+export WORKLOAD_NAME=${USER}-xpk-${TPU_TYPE}-nov261457pm # Your workload name. Need to update for different run.
 export BASE_DOCKER_IMAGE=us-central1-docker.pkg.dev/deeplearning-images/reproducibility/pytorch-tpu-mixtral:v1
-export PROFILE_LOG_DIR=... # GCS bucket to store profile in form of gs://...
-export HF_TOKEN=... # Add your own Hugging face token to download model
+export PROFILE_LOG_DIR=gs://manfei_bucket_automated # GCS bucket to store profile in form of gs://...
+export HF_TOKEN=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 # Add your own Hugging face token to download model
